58fe015923bac201f7263e76328893004749868a
kyrstinlawson <kyrstin.lawson@gmail.com>
2022-07-13 17:06:45 -0700

    Changes for loop to .map

--- js/scripts.js
+++ js/scripts.js
@@ -11,20 +11,20 @@ fetch("https://randomuser.me/api/?results=12")
     });
 
 function generateCard(data) {
-    for (let i=0; i < data.length; i++) {
-        let html = `
+    data.map(person => {
+        const html = `
             <div class="card">
                 <div class="card-img-container">
-                    <img class="card-img" src="${data[i].picture.medium}" alt="profile picture">
+                    <img class="card-img" src="${person.picture.large}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
-                    <p class="card-text">${data[i].email}</p>
-                    <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
+                    <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                    <p class="card-text">${person.email}</p>
+                    <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
                 </div>
             </div>
         `;
 
         gallery.insertAdjacentHTML("beforeend", html);
-    }
+    });
 };
